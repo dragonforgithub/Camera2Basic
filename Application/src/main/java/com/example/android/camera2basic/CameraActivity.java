8db738a8c806b51da987ead270882c7b14860a38
sheldon <dragon1992long@163.com>
2019-03-19 15:40:51 +0800

    整合拍照和录像功能

--- Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
+++ Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
@@ -16,20 +16,79 @@
 
 package com.example.android.camera2basic;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 public class CameraActivity extends AppCompatActivity {
+
+    private final int PICTURE_MODE = 0;
+    private final int VIDEO_MODE = 1;
+    private int mCurMode = 0;
+
+    private ImageButton mModeButton = null;
+    private Context mContext = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = this;
         setContentView(R.layout.activity_camera);
-        if (null == savedInstanceState) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, Camera2BasicFragment.newInstance())
-                    .commit();
-        }
+
+        mModeButton = findViewById(R.id.mode_switch);
+        mModeButton.setOnClickListener(new ButtonPart());
+
+        //默认启动拍照模式
+        switch_to_pictureMode();
     }
 
+    /**
+     * 切换到录像模式
+     * */
+    private void switch_to_pictureMode(){
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, Camera2PictureFragment.newInstance())
+                .commit();
+        mCurMode = PICTURE_MODE;
+        Drawable pic_drawable = ContextCompat.getDrawable(mContext, R.drawable.btn_switch_to_video);
+        mModeButton.setBackground(pic_drawable);
+    }
+
+    /**
+     * 切换到拍照模式
+     * */
+    private void switch_to_videoMode(){
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, Camera2VideoFragment.newInstance())
+                .commit();
+        mCurMode = VIDEO_MODE;
+        Drawable video_drawable = ContextCompat.getDrawable(mContext, R.drawable.btn_switch_to_picture);
+        mModeButton.setBackground(video_drawable);
+    }
+
+    /**
+     * 按键事件处理
+     * */
+    class ButtonPart implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+                case R.id.mode_switch:
+                    if (mCurMode == PICTURE_MODE) {
+                        switch_to_videoMode();
+                    } else if (mCurMode == VIDEO_MODE) {
+                        switch_to_pictureMode();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
